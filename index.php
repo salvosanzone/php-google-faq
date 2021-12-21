65207b1f3063f49ff5871e1834ad7aa4a809f230
salvosanzone <salvosansone.ss@gmail.com>
2021-12-21 14:16:27 +0100

    creo un layout di base

--- index.php
+++ index.php
@@ -26,10 +26,14 @@ $faq = [
     Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
     
     Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.'
+  ],
+  [
+    'question' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
+    'answer' => 'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell URL referrer potrebbero essere disponibili mediante Google Analytics o un API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all esatte parole chiave che hanno determinato il clic su un annuncio.'
   ]
   ];
-
-  var_dump($faq);
+  
+  //var_dump($faq);
 
   foreach($faq as $singleFaq){
     echo '<hr>';
